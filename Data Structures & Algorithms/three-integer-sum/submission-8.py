af11cc7015d5be9e28d2c02773f06f3c50894558
Irene Cheung <144499931+izcheung@users.noreply.github.com>
2026-09-03 01:40:52 -0600

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # all three numbers equal 3 and are all different indices
+        # Must sort the array first
+        # can use hash map to find the corresponding target value,
+        # have a for loop set one of the indices
+        # I have to be careful about the any order
+
+        seen = {}
+        nums.sort()
+        combinations = set()
+        ans = []
+        for i in range(len(nums)):
+            j = i + 1
+            k = len(nums)-1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k] 
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                else:
+                    combo = tuple([nums[i], nums[j], nums[k]])
+                    if combo not in combinations:
+                        combinations.add(combo)
+                        ans.append([nums[i], nums[j], nums[k]])
+                    j += 1
+        return ans
