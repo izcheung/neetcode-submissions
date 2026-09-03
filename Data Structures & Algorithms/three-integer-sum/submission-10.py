ea8ed8c547d5fba59cff140aa5dac3ae99f9a85b
Irene Cheung <144499931+izcheung@users.noreply.github.com>
2026-09-03 08:05:49 -0600

    Add: three-integer-sum - submission-10

---
+++ Data Structures & Algorithms/three-integer-sum/submission-10.py
@@ -0,0 +1,18 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # use a for loop on the outside for the first number
+        # Use a second for loop for the second variable use a hashmap to keep track of what you have seen so far
+        result = []
+        nums.sort()
+        for i in range(len(nums)):
+            seen = {}
+            for j in range(i+1, len(nums)):
+                need = 0 - nums[i] - nums[j]
+                if need in seen:
+                    combo = [nums[i], need, nums[j]]
+                    if combo not in result:
+                        result.append(combo)
+                else:
+                    seen[nums[j]] = j
+        return result
+                    
