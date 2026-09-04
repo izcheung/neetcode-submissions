ef9fb703b92a413755b16fa98f4a2ccfbcd5a43b
Irene Cheung <144499931+izcheung@users.noreply.github.com>
2026-09-04 15:02:05 -0600

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,46 @@
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+
+        mapOne = Counter(s1)
+        mapTwo = {}
+
+        '''
+        s1 = "abc", 
+
+        lecabee
+         l r 
+        
+        mapOne = {a: 1, b:1, c:1}
+        mapTwo = {e:1, c:1, a:1}
+
+        '''
+
+        l = 0
+        for r in range(len(s2)):
+            rightChar = s2[r]
+            mapTwo[rightChar] = 1 + mapTwo.get(rightChar, 0)
+
+            window_size = r - l + 1
+
+            if window_size < len(s1):
+                continue
+            elif window_size == len(s1):
+                if mapOne == mapTwo:
+                    return True
+            else:
+                mapTwo[s2[l]] -= 1
+                if mapTwo[s2[l]] == 0:
+                    mapTwo.pop(s2[l])
+                l += 1
+                if mapOne == mapTwo:
+                    return True
+        return False
+            
+
+            
+
+
+
